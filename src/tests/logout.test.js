e52041ca8516d89a8f3a2c648aa84c0fcf00dcb3
Tanix98 <oysteinrostvik@protonmail.com>
2022-12-16 23:23:54 +0100

    create cypress tests

--- src/tests/logout.test.js
+++
@@ -1,9 +0,0 @@
-import { logout } from '../js/api/auth/logout.js';
-
-describe('logout function', () => {
-  it('Clears the token from browser storage', () => {
-    logout();
-    const accessToken = localStorage.getItem('accessToken');
-    expect(accessToken).toBeFalsy();
-  });
-});
